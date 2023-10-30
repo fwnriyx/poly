6ae21ab0307df7720405c5a54e41b4647df5e68e
2b06.2222811.Fitri_Amir <forschoolwork.exe@gmail.com>
2023-10-30 13:38:00 +0800

    Sorted files

--- Data Structures and Algorithms/CA1Trial/misc.py
+++ Data Structures and Algorithms/CA1Trial/misc.py
@@ -19,5 +19,37 @@ class misc:
             print('* ' + ' ' * padding + line + ' ' * (max_line_length - len(line) - padding) + ' *')
         
         print('*' * box_width)
-
     
+    def numericalCheck(choice):
+        try:
+            int(choice)
+            return True
+        except ValueError:
+            return False
+
+    def menu(self):
+        self.__print_self()
+        while True:
+            starting_choices = ['New', 'Open', 'Sort', 'Process Text', 'Text Summarization', 'Text Searching','Print', 'Save', 'Save as', 'Exit']
+            user_choice = Utils.get_number_choice(arr_choices=starting_choices)
+            if user_choice == 1:
+                self.__choice1()
+            elif user_choice == 2:
+                self.__choice2()
+            elif user_choice == 3:
+                self.__choice3()
+            elif user_choice == 4:
+                self.__choice4()
+            elif user_choice ==5:
+                self.__choice5()
+            elif user_choice == 6:
+                self.__choice6()
+            elif user_choice == 7:
+                self.__choice7()
+            elif user_choice == 8:
+                self.__choice8()
+            elif user_choice == 9:
+                self.__choice9()
+            elif user_choice == 10:
+                self.__choice10()
+                return
---
+++ Deep Learning/CA1/README.md
@@ -0,0 +1,17 @@
+In this project, there are 2 parts:
+    1: Implement an image classifier using a deep learning network. You are given colour images of 224 by 224 pixels, containing 15 types of vegetables.
+    2: Build a next-word predictor, given a sequence of words.
+
+In part 1:
+    Features I considered:
+        -Augmentation (Class balancing)
+        -Parameter Tuning (Best hyperparameters)
+        -Model architecture (Efficiency and effectiveness)
+        -Generalization (prevent overfitting)
+        -Regularization
+```python
+        
+```
+
+
+In part 2:
